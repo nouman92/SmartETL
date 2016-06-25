a7c2fe2c2b2010ca46a72daa186a5710b1cc3c29
nouman <nouman92@live.com>
2016-06-26 04:13:41 +0500

    initial commit

---
+++ src/core/LocalModule.java
@@ -0,0 +1,12 @@
+package core;
+/*
+ * LocalModule.java
+ *
+ * Herbert Laroca, 2005
+ */
+
+public interface LocalModule {
+    
+    public String[] transform(MiniETL oetl, String strSeq);
+           
+} 
